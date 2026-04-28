3efb539257a696e5e7666c212fc6421805b5e96d
Loki <yongxiang.zhan@outlook.com>
2026-04-28 15:31:39 +0800

    feat: initial commit

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,30 @@
+import { Geist, Geist_Mono } from "next/font/google"
+
+import "@workspace/ui/globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
+import { cn } from "@workspace/ui/lib/utils";
+
+const fontSans = Geist({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+const geistMono = Geist_Mono({subsets:['latin'],variable:'--font-mono'})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn("antialiased", fontSans.variable, "font-mono", geistMono.variable)}
+    >
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  )
+}
